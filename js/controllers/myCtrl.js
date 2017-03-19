8c6da3e5b3abf561d0a035af5df60a78a96b7459
Jules1 <julian.i@live.com>
2017-03-19 14:36:39 -0500

    error handling

--- js/controllers/myCtrl.js
+++ js/controllers/myCtrl.js
@@ -3,4 +3,7 @@ app.controller("myCtrl", function($scope) {
     $scope.addItem = function () {
         $scope.products.push($scope.addMe);
     }    
+    $scope.removeItem = function (x) {
+        $scope.products.splice(x, 1);
+    }
 });
